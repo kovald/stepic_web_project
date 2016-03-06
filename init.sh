48cc9b5e45b39297843ddb8db1e02f9659b59f78
dmiras@Yandex.ru <dmiras@yandex.ru>
2016-03-06 19:42:21 +0300

    all222

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo ﻿ln -s /etc/nginx/nginx.conf  /home/box/web/etc/nginx.conf
+sudo rm  /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
+sudo ln -s /etc/nginx/sites-available/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
