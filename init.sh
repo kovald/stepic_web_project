1f2c112445f4822c0e3505d8093f0fc4389d9d69
Dmitriy Koval <kovalds@yandex.ru>
2016-05-15 17:49:52 +0300

    end

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
-sudo rm  /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
-sudo ln -s /etc/nginx/sites-available/test.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+# sudo apt-get update
+# sudo apt-get install -y nginx
+# mkdir -p /home/box/web/public/{img,css,js}
+# mkdir -p /home/box/web/{uploads,etc}
+
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo nginx -t && sudo /etc/init.d/nginx restart
+sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
